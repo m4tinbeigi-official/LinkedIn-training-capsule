2a2fffd1981f4b087d04dfdfa7985151f7b24c55
Rick Sanchez <101627789+m4tinbeigi-official@users.noreply.github.com>
2025-01-05 07:37:23 +0330

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    fetch('assets/data/data.json')
+    fetch('data.json')
         .then(response => response.json())
         .then(data => {
             const content = document.getElementById('content');
